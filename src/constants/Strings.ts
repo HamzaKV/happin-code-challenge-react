8284e3dceaeda3930bc673055878ce3cf7e60f65
HamzaKV <hamzakv333@gmail.com>
2021-07-03 12:31:24 -0400

    Update Strings.ts

--- src/constants/Strings.ts
+++ src/constants/Strings.ts
@@ -1,4 +1,58 @@
 // eslint-disable-next-line import/no-anonymous-default-export
-export default ({
-    appName: 'App'
-});
+export default {
+    appName: 'App',
+    cities: [
+        'san jose',
+        'santiago',
+        'san francisco',
+        'santa rosa',
+        'san juan',
+        'sabadell',
+        'salamanca',
+        'salt lake city',
+        'salinas',
+        'salem',
+        'sausalito',
+        'taipei',
+        'tel aviv',
+        'tempe',
+        'termez',
+        'temuco',
+        'tiajuna',
+        'tieling',
+        'thousand oaks',
+        'thunder bay',
+        'tokyo',
+        'tulsa',
+    ],
+    books: [
+        {
+          title: 'Don Quixote',
+          author: 'Miguel De Cervantes',
+        },
+        {
+          title: 'Pilgrim\'s Progress',
+          author: 'John Bunyan',
+        },
+        {
+          title: 'Robinson Crusoe',
+          author: 'Daniel Defoe',
+        },
+        {
+          title: 'Gulliver\'s Travels',
+          author: 'Jonathan Swift',
+        },
+        {
+          title: 'Tom Jones',
+          author: 'Henry Fielding',
+        },
+        {
+          title: 'Clarissa',
+          author: 'Samuel Richardson',
+        },
+        {
+          title: 'Tristram Shandy',
+          author: 'Laurence Sterne',
+        },
+      ]
+};
